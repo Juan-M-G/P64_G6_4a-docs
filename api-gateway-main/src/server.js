ace4256e0985d769e5b2584ce7f3cb4e2e98461e
JuanMayorga <juank-963@hotmail.com>
2021-12-12 14:38:55 -0500

    Add apigateway and customers ms

---
+++ api-gateway-main/src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    auth_api_url: 'https://auth-ms-p64-g6.herokuapp.com',
+    customer_api_url: 'https://ciclo4-grupo6-customerms.herokuapp.com',
+    bill_api_url: 'https://bill-ms.herokuapp.com',
+}
